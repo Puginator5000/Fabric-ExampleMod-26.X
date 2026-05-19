9d907837e33718b8686e54d8adb70608c82d2217
Puginator5000 <emmaminime3662@gmail.com>
2026-05-19 00:03:17 -0700

    Initial commit

---
+++ src/main/java/net/puginator5000/examplemod/ExampleMod.java
@@ -0,0 +1,17 @@
+package net.puginator5000.examplemod;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class ExampleMod implements ModInitializer {
+	public static final String MOD_ID = "examplemod";
+	
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/net/puginator5000/examplemod/ExampleModDataGenerator.java
@@ -0,0 +1,11 @@
+package net.puginator5000.examplemod;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class ExampleModDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
